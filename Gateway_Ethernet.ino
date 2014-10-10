871af46dc71825bca34b2a74172efc7ab237aeb0
demondreamer <demondreamer@users.noreply.github.com>
2014-10-10 18:08:39 -0500

    Update Gateway_Ethernet.ino
    Removed most MQTT connection watchdog code in favor of a method I found at https://github.com/knolleary/pubsubclient/issues/41 . I haven't tested it extensively yet but it seems to be simpler and hopefully more stable.
    
    Commented the heck out of everything
    
    Also added a functional MQTT subscription ability. I think it's a good first step towards making this setup bi-directional

--- Gateway_Ethernet.ino
+++ Gateway_Ethernet.ino
@@ -1,14 +1,17 @@
 /*
 Author:  Eric Tsai
 License:  CC-BY-SA, https://creativecommons.org/licenses/by-sa/2.0/
-Date:  7-21-2014
+Date:  10-10-2014
 File: Ethernet_Gateway.ino
 This sketch takes data struct from I2C and publishes it to
-mosquitto broker.
+mosquitto broker. It also subscribes to topics and outputs 
+the topic and message to the serial monitor.
 
 Modifications Needed:
 1)  Update mac address "mac[]"
 2)  Update MQTT broker IP address "server[]"
+3.  Update the IP address of this device
+4.  Update the 2 subscription areas
 */
 
 #include <SPI.h>
@@ -17,61 +20,81 @@ Modifications Needed:
 #include <PubSubClient.h>
 
 //I2C receive device address
-const byte MY_ADDRESS = 42;    //I2C comms w/ other Arduino
+const byte MY_ADDRESS = 43;    //I2C comms w/ other Arduino
 
 //Ethernet
-byte mac[]    = {  0x90, 0xA2, 0xDA, 0x0D, 0x11, 0x11 };
-byte server[] = { 192, 168, 1, 51 };
+byte mac[]    = {  0x90, 0xA2, 0xDA, 0x0D, 0x11, 0x12 }; // assign mac address to this device. It can be anything, use the one on your ethernet shield if it has one, otherwise make it up. Just make sure it's unique on your network.
+byte server[] = { 192, 168, 1, 143 }; //IP address of the MQTT server. 
+IPAddress ip(192,168,1,107); //IP address of this device
 
-IPAddress ip(192,168,2,61);
+
+//EthernetClient ethClient;
 EthernetClient ethClient;
 PubSubClient client(server, 1883, callback, ethClient);
-unsigned long keepalivetime=0;
-unsigned long MQTT_reconnect=0;
 
+// Used to convert millis() to seconds, for debugging MQTT connection status
+unsigned long time=0;
 
-//use LED for indicating MQTT connection status.
-int led = 13;
-bool conn_ok;
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  // handle message arrived
+  // MQTT Callback function. This is where the programming goes to respond to payloads on topics you've subscribed to.
+  // currently it just prints the topic and payload out to the serial monitor.
+  // TODO: figure out how to read the payload, parse it out to NodeID, DeviceID, Command, and send it via I2c to the RFM
+  //       gateway to be radio transmitted to the nodes.
+void callback(char* topic, byte* payload, unsigned int length)
+{
+  for (int i=0; i<length; i++) {
+    Serial.print((char)payload[i]);
+  }
+    Serial.println();
 }
 
 
 void setup() 
 {
-  //ethernet
-  //Ethernet.begin(mac, ip);
-  Wire.begin (MY_ADDRESS);
-  Serial.begin (9600);
+
+  Wire.begin (MY_ADDRESS);                        // open communication to the RFM gateway over I2c
+  Serial.begin (9600);                            // open the serial connection to the console for debugging
   
-  Serial.println("starting");
-  
-  pinMode(led, OUTPUT);
+        Serial.println("starting... long delay means problem with ethernet");
   
   //wait for IP address
+    while (Ethernet.begin(mac) != 1)              // while there's no ethernet connection, throw an error message and wait 3 seconds
+      {
+        Serial.println("Error getting IP address via DHCP, trying again...");
+        delay(3000);
+      }
   
-  while (Ethernet.begin(mac) != 1)
+        Serial.println("ethernet OK");
  
-  {
-    Serial.println("Error getting IP address via DHCP, trying again...");
-    delay(3000);
-  }
-  
-  //Ethernet.begin(mac, ip);
-  Serial.println("ethernet OK");
-  keepalivetime=millis();
-  Wire.onReceive (receiveEvent);
+  Wire.onReceive (receiveEvent);                  // On receiving i2c data, call receiveEvent interrupt function at the end of sketch to process incomming data 
+   
+  while (client.connect("arduinoClient") != 1)    // while there's no MQTT connection, throw an error message and wait 3 seconds
+      {
+        Serial.println("Error connecting to MQTT");
+        delay(3000);
+      }
+        Serial.println("setup complete");
 
-  while (client.connect("arduinoClient") != 1)
-  {
-    Serial.println("Error connecting to MQTT");
-    delay(3000);
-  }
-  MQTT_reconnect = millis();
-  Serial.println("setup complete");
+
+// ====================================================================================     
+// Enter MQTT subscriptions here and also down in the loop.
+// The ones here will be active as soon as void setup finishes, until the MQTT reconnection happens
+// P.S. use "mosquitto_sub -h 192.168.xxx.xxx -t "#" -v" command line to view all topics in mosquitto
+
+
+        client.subscribe("1111");
+        client.subscribe("2222");
+        client.subscribe("3333");
+        client.subscribe("4444");
+
+        client.publish("Arduino","Ethernet MQTT Gateway Up");
+        
+      
+    
   
+
+// ====================================================================================     
+
+
 }  // end of setup
 
 
@@ -91,68 +114,66 @@ volatile boolean haveData = false;
 
 void loop() 
 {
-  
-  if (haveData)
-  {
-    Serial.print ("Received Device ID = ");
-    Serial.println (SensorNode.sensorID);  
-    Serial.print ("    Time = ");
-    Serial.println (SensorNode.var1_usl);
-    Serial.print ("    var2_float ");
-    Serial.println (SensorNode.var2_float);
+   if (!client.loop()) 
+     {
+ 
+       Serial.print("Client disconnected...");
+
+      if (client.connect("arduinoPublisher"))
+        {
+          Serial.print("reconnected.");
+// ====================================================================================     
+// Enter MQTT subscriptions here also.
+// These will be refreshed when the MQTT client disconnect/reconnect happens and so stay responsive
+// P.S. use "mosquitto_sub -h 192.168.xxx.xxx -t "#" -v" command line to view all topics in mosquitto
+
+
+        client.subscribe("1111");
+        client.subscribe("2222");
+        client.subscribe("3333");
+        client.subscribe("4444");
+
+        client.publish("Arduino","Ethernet MQTT Gateway Up");
+        
+      
     
-    sendMQTT = 1;
-    
-    /*
-    if (client.connect("arduinoClient"))
-    {
-      int varnum = 1;
-      char buff_topic[6];
-      char buff_message[6];
-      sprintf(buff_topic, "%02d%01d%01d", SensorNode.nodeID, SensorNode.sensorID, varnum);
-      Serial.println(buff_topic);
-      dtostrf (SensorNode.var2_float, 4, 1, buff_message);
-      client.publish(buff_topic, buff_message);
-    }
-    */
-    
-    haveData = false;
-  }  // end if haveData
   
+
+// ====================================================================================     
+        } 
+      
+      else
+        {
+          Serial.print("failed.");
+            time = millis();
+            Serial.println(time/1000);
+        }
+    
   
-  if (sendMQTT == 1)
+     }                                // end if client.loop
+   
+     
+  if (haveData)                      // "recieveEvent" interrupt routine sets this "haveData" variable true when it's complete so the code progresses beyond it
   {
 
-      Serial.println("starting MQTT send");
-      
-      conn_ok = client.connected();
-      if (conn_ok==1)
-      {
-        digitalWrite(led, HIGH);
-        Serial.println("MQTT connected OK from MQTT Send");
-      }
-      else
-      {
-        digitalWrite(led, LOW);
-        Serial.println("MQTT NOT connected OK from MQTT Send");
-      }
-      
-      //no connection, reconnect
-      if (conn_ok == 0)
-      {
-        client.disconnect();
-        delay(5000);
-        while (client.connect("arduinoClient") != 1)
-        {
-          digitalWrite(led, LOW);
-          Serial.println("Error connecting to MQTT");
-          delay(4000);
-          digitalWrite(led, HIGH);
-        }
-        digitalWrite(led, HIGH);
-        Serial.println("reconnected to MQTT");
-        MQTT_reconnect = millis();
-      }
+    Serial.println ();
+    Serial.print ("Received Node ID      = ");
+    Serial.println (SensorNode.nodeID);  
+    Serial.print ("Received Device ID    = ");
+    Serial.println (SensorNode.sensorID);  
+    Serial.print ("1. Time               = ");
+    Serial.print (SensorNode.var1_usl);
+    Serial.print (" millis, ");
+    Serial.print (SensorNode.var1_usl/1000);
+    Serial.print (" seconds, or ");
+    Serial.print ((SensorNode.var1_usl / 1000) /60);
+    Serial.println (" minutes");
+    Serial.print ("2. var2_float         = ");
+    Serial.println (SensorNode.var2_float);
+    Serial.print ("3. var3_float         = ");
+    Serial.println (SensorNode.var3_float);    
+    Serial.print ("4. RSSI               = ");
+    Serial.println (SensorNode.var4_int); 
 
 
       int varnum;
@@ -160,85 +181,56 @@ void loop()
       char buff_message[12];      
 
       
-      /*
-      //send var1_usl
-      varnum = 2;
+    Serial.println (); 
+
+      //send var1_usl - This is used to pass millis() value at the time the sensor reading was transmitted
+      varnum = 1;
       buff_topic[6];
       buff_message[12];
       sprintf(buff_topic, "%02d%01d%01d", SensorNode.nodeID, SensorNode.sensorID, varnum);
+      Serial.print("MQTT publish topic: "); 
       Serial.println(buff_topic);
       dtostrf (SensorNode.var1_usl, 10, 1, buff_message);
       client.publish(buff_topic, buff_message);
-      */
       
-      //send var2_float
+      
+      //send var2_float - sensor data
       varnum = 2;
       buff_topic[6];
       buff_message[7];
       sprintf(buff_topic, "%02d%01d%01d", SensorNode.nodeID, SensorNode.sensorID, varnum);
+      Serial.print("MQTT publish topic: "); 
       Serial.println(buff_topic);
       dtostrf (SensorNode.var2_float, 2, 1, buff_message);
       client.publish(buff_topic, buff_message);
       
       delay(200);
       
-      //send var3_float
+      //send var3_float - sensor data
       varnum = 3;
       sprintf(buff_topic, "%02d%01d%01d", SensorNode.nodeID, SensorNode.sensorID, varnum);
+      Serial.print("MQTT publish topic: "); 
       Serial.println(buff_topic);
       dtostrf (SensorNode.var3_float, 2, 1, buff_message);
       client.publish(buff_topic, buff_message);
 
       delay(200);
       
-      //send var4_int, RSSI
+      //send var4_int, RSSI - This passess on the node's RSSI at the time of sending 
       varnum = 4;
       sprintf(buff_topic, "%02d%01d%01d", SensorNode.nodeID, SensorNode.sensorID, varnum);
+      Serial.print("MQTT publish topic: "); 
       Serial.println(buff_topic);
       sprintf(buff_message, "%04d%", SensorNode.var4_int);
       client.publish(buff_topic, buff_message);
-      
-      sendMQTT = 0;
-      Serial.println("finished MQTT send");
-  }//end if sendMQTT
-  
-  
-  //client.loop needs to run every iteration.  Previous version did not.  Big opps.
-  client.loop();
-  
-  
-  if ((millis() - MQTT_reconnect) > 60000)
-  {
-    conn_ok = client.connected();
-    if (conn_ok==1)
-    {
-      digitalWrite(led, HIGH);
-      Serial.println("MQTT connected OK");
-    }
-    else
-    {
-      digitalWrite(led, LOW);
-      Serial.println("MQTT NOT connected OK");
-    }
-    
-    //no connection, reconnect
-    if (conn_ok == 0)
-    {
-      client.disconnect();
-      delay(5000);
-      while (client.connect("arduinoClient") != 1)
-      {
-        digitalWrite(led, LOW);
-        Serial.println("Error connecting to MQTT");
-        delay(4000);
-        digitalWrite(led, HIGH);
-      }
-      digitalWrite(led, HIGH);
-    }
 
-    Serial.println("reconnected to MQTT");
-    MQTT_reconnect = millis();
-  }
+      Serial.println ();
+      Serial.println("finished MQTT send");
+      Serial.println ();
+          
+  haveData = false;  // set "haveData" false since the data taken from I2C, parsed by the recieveEvent interrupt, has been published to MQTT
+  
+}  // end if haveData
 
 }  // end of loop
 
@@ -256,5 +248,5 @@ void receiveEvent (int howMany)
   for (byte i = 0; i < sizeof SensorNode; i++)
     *p++ = Wire.read ();
     
-  haveData = true;     
+  haveData = true;     // once "haveData" is set true, the sketch will proceed to publish the data to MQTT
 }
